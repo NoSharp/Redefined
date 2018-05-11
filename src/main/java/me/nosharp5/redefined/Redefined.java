4875ca4fc2d39ea6487454fce51d322c89d4a781
Harry <DawnServerOwner@gmail.com>
2018-05-11 18:19:23 +0100

    Start of the saga.

---
+++ src/main/java/me/nosharp5/redefined/Redefined.java
@@ -0,0 +1,17 @@
+package me.nosharp5.redefined;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Redefined extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
